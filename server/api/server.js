89a1ab92e7385ecbe324cd58c216c886efe50fcd
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-01-30 14:25:09 +0100

    feat: authentication work fine

--- server/api/server.js
+++ server/api/server.js
@@ -11,12 +11,8 @@ connectDB(); // connect to db
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 app.use("/user", require("./routes/userRoutes"));
 
